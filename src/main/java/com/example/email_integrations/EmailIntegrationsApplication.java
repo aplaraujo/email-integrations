db3b2fa37a42899f7fc187f50d7de05f93aaa84a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-21 20:31:32 -0300

    first commit

---
+++ src/main/java/com/example/email_integrations/EmailIntegrationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_integrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailIntegrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailIntegrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/email_integrations/EmailIntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email_integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailIntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
